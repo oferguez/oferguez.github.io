9b0bba242f70d5e21b5a63300bdd7c7679e13ed2
Ofer Guez <guez.ofer@gmail.com>
2025-09-08 12:37:39 +0100

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/landing/',
   server: {
     port: 3000,
   },
