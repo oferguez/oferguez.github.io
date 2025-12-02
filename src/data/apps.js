1d85e03b1aebf149fea59ded53699d8e53e7482b
Ofer Guez <guez.ofer@gmail.com>
2025-12-02 14:51:15 +0000

    m with squirrels

--- src/data/apps.js
+++ src/data/apps.js
@@ -8,6 +8,14 @@ export const apps = [
     endOfLine: true
   },
   {
+    id: 'maze-generator',
+    title: `Shira's Maze Game`,
+    description: 'Create rectangular mazes with specific row/column sizes and optional seeds.',
+    tags: ['Games', 'Puzzles', 'React'],
+    path: '/maze',
+    endOfLine: false
+  },
+  {
     id: 'even-path-finder',
     title: 'Even Path Finder',
     description: 'Find the path through a grid with even/odd/triple weights.',
@@ -53,14 +61,6 @@ export const apps = [
     description: 'Calculate win rates and determine how many wins or losses are needed to change your percentage. Enter any 2 values to auto-calculate the third.',
     tags: ['Statistics', 'Gaming', 'Calculator'],
     path: '/rate-calculator',
-    endOfLine: false
-  },
-  {
-    id: 'maze-generator',
-    title: 'Maze Generator',
-    description: 'Create rectangular mazes with specific row/column sizes and optional seeds.',
-    tags: ['Games', 'Puzzles', 'React'],
-    path: '/maze',
     endOfLine: false
   },
   {
